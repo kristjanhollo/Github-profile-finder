cbbf0c7537f037ca39973b462462d358fa6f82cf
kristjanhollo <hollo.kristjan@gmail.com>
2022-01-03 20:47:34 +0200

    [add] - React-router-dom, React Icons, Nav-bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import React from 'react';
+
 
 function App() {
   return (
-    <div>
-     <h1>Hello world!</h1>
-     
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main>
+          Content
+        </main>
+      </div>
+
+    </Router>
   );
 }
 
