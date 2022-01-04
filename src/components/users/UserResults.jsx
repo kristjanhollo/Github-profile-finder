9c512e260b0a3d6f55d8d8f559489b88c06f1d40
kristjanhollo <hollo.kristjan@gmail.com>
2022-01-04 21:48:35 +0200

    [add] - Spinner while fetching users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Spinner from '../layout/Spinner';
 
 
 
@@ -16,7 +17,7 @@ function UserResults() {
   const fetchUsers = async () => {
     const response = await fetch('https://api.github.com/users', {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+        Authorization: 'token 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'
       }
     });
 
@@ -28,12 +29,12 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <h3 key={user.id}>{user.login}</h3>
         ))}
       </div>
     )
   } else {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
   
 }
