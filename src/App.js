41ed9addc12c5b7aba753e020c373f4d3ff0a539
kristjanhollo <hollo.kristjan@gmail.com>
2022-01-04 22:34:47 +0200

    [add] - Context handler

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import React from 'react';
+import {GithubProvider} from './context/github/GithubContext';
 
 
 function App() {
   return (
+    <GithubProvider>
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
@@ -26,6 +28,7 @@ function App() {
       </div>
 
     </Router>
+    </GithubProvider>
   );
 }
 
